222352ab38dcc65ecdb899d895f64e55daadf10d
unknown <@-.(none)>
2012-04-02 01:23:40 +0400

    live

--- test.js
+++ test.js
@@ -1,19 +1,27 @@
 $(document).ready(function(){
     // добавим обработчик события hover
-    $('.editable').hover(
+	$('.editable').live('mouseover', function () {
+			$('.editable').removeClass('active');
+			$(this).addClass('active');
+			return false;
+    });
+	$('.editable').live('mouseout', function () {
+			$(this).removeClass('active');
+			$(this).parent('.editable').addClass('active');
+			return false;
+    });
+    /*$('.editable').hover(
         function () {
 			$('.editable').removeClass('active');
 			$(this).addClass('active');
-			/*if ($(this).children('.edit-btn').first.length == 0){
-				$(this).append('<div class = "edit-btn">Edit</div>');
-				} else {
-					$(this).children('.edit-btn').show();
-				}*/
         },
         function() {
             $(this).removeClass('active');
-			//$(this).children('.edit-btn').hide();
 			$(this).parent('.editable').addClass('active');
         }
-    );
+    );*/
+	var divOnClick = function(_e) {
+        console.log('div clicked');
+      }
+    $('.active').live('click', divOnClick);
 });
