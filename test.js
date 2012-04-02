e26edb20bae81eaa7fdc634d20d1e41d7133e05d
unknown <@-.(none)>
2012-04-03 03:28:47 +0400

    old

--- test.js
+++ test.js
@@ -3,33 +3,30 @@ $(document).ready(function(){
 	$('body').append('<div id="dialogMain"></div>');
     // delegate the mouseover, mouseout events handler
 	$('.editable').live('mouseover', function () {
-			$('.editable').removeClass('active');
-			$('.edit-btn').hide();
-			$(this).addClass('active');
-			$(this).children('.edit-btn').show();
-			return false;
+		$('.editable').removeClass('active');
+		$('.edit-btn').hide();
+		$(this).addClass('active');
+		$(this).children('.edit-btn').show();
+		return false;
     });
 	$('.editable').live('mouseout', function () {
-			$('.edit-btn').hide();
-			$(this).removeClass('active');
-			$(this).parent('.editable').addClass('active');
-			$(this).parent('.editable').children('.edit-btn').show();
-			return false;
+		$('.edit-btn').hide();
+		$(this).removeClass('active');
+		$(this).parent('.editable').addClass('active');
+		$(this).parent('.editable').children('.edit-btn').show();
+		return false;
     });
     $('.edit-btn').live('click', function() {
 		modalInit();
     });
-	$('#closebtn').live('click', function() {
+	$('#applybtn').live('click', function() {
 		$('#dialogMain').animate({ opacity: "hide" }, "fast");;
 	})
 });
 function modalInit(){
 	if ($('#dialogMain').find('#modal').length == 0) {
 		$('#dialogMain').load('/Meruert/themes/main-test/modal.html');
-	} else {
-		//('#dialog#modal').show();
-		console.log('ururu');
+		$('#dialogMain').hide();
 	}
-	$('#dialogMain').hide();
 	$('#dialogMain').animate({ opacity: "show" }, "medium");
 }
